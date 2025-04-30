9755188f275dd148b30d008d54c938d823334318
Juanluis439 <juanluisdelacruz439@gmail.com>
2025-04-30 10:35:08 +0200

    Día de la madre

--- wsp.js
+++ wsp.js
@@ -2,7 +2,7 @@ function enviarWsp() {
   const telefono = "393409691101";
 
   let mensaje =
-    " ciao, vorrei creare insieme a te il set di Gioielli perfetto per mi mamma";
+    " Ciao, vorrei creare insieme a te il set di gioielli perfetto per mia mamma";
 
   const enlaceWsp =
     "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
